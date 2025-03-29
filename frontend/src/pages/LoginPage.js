d53b3bdf0dc39e719d89e0d4b19c0b5aa8597a9f
Yash-ai-bit <gujjaryash7089@gmail.com>
2025-03-29 18:11:15 +0000

    styles matched to theme!

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -6,34 +6,22 @@ import "./LoginPage.css";
 import { Link, useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
-    const [cookies, setCookie] = useCookies(["jwt"]);
-    const [loading, setLoading] = useState(true);
-    const navigate = useNavigate();
-    const pageStyle = {
-        display: "flex",
-        height: "100vh",
-        width: "100%",
-        justifyContent: "center",
-        alignItems: "center",
-        background: "rgb(236, 236, 238)",
-        overflow: "hidden",
-        fontFamily: "Verdana, Geneva, Tahoma, sans-serif",
-    };
-    
-    useEffect(() => {
+  const [cookies] = useCookies(["jwt"]);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
-      console.log("Cookies:", cookies.jwt); // Debugging
-    
-      if (loading) {
-        setTimeout(() => setLoading(false), 100); // Delay checking cookies
-        return;
-      }
-    
-      if (cookies.jwt) {
-        navigate("/home");
-      }
-    }, []);
+  useEffect(() => {
+    console.log("Cookies:", cookies.jwt); // Debugging
 
+    if (loading) {
+      setTimeout(() => setLoading(false), 100); // Delay checking cookies
+      return;
+    }
+
+    if (cookies.jwt) {
+      navigate("/home");
+    }
+  }, []);
 
   const [values, setValues] = useState({
     email: "",
@@ -70,40 +58,40 @@ const LoginPage = () => {
   };
 
   return (
-    <div style={pageStyle}>
-      <div className="logincontainer">
-        <h2>Login Account</h2>
-        <form onSubmit={(e) => handleSubmit(e)}>
-          <div>
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              name="email"
-              placeholder="Email"
-              onChange={(e) =>
-                setValues({ ...values, [e.target.name]: e.target.value })
-              }
-            />
-          </div>
-          <div>
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              placeholder="Password"
-              name="password"
-              onChange={(e) =>
-                setValues({ ...values, [e.target.name]: e.target.value })
-              }
-            />
-          </div>
-          <button type="submit">Submit</button>
-          <span>
-            Create an Account?!<Link to="/"> Register</Link>
-          </span>
-        </form>
-        <ToastContainer />
+    <div className="login-page-container">
+        <div className="logincontainer">
+          <h2>Login Account</h2>
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                onChange={(e) =>
+                  setValues({ ...values, [e.target.name]: e.target.value })
+                }
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                onChange={(e) =>
+                  setValues({ ...values, [e.target.name]: e.target.value })
+                }
+              />
+            </div>
+            <button type="submit">Submit</button>
+            <span>
+              Create an Account?!<Link to="/"> Register</Link>
+            </span>
+          </form>
+          <ToastContainer />
+        </div>
       </div>
-    </div>
   );
 };
 
